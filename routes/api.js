222654b5794fa663e7688cedeb31351704a645bb
Benjamin Netanyahu <beniiboii00@gmail.com>
2021-10-09 21:33:21 +1100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log("connected succesfully");
-});
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   console.log("connected succesfully");
+// });
 
 console.log(mongoose.connection.readyState);
 
